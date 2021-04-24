0d8ed20a31b2fe0d69977d6c02d7038b5d4c7450
Alessandro Coretti <alessandro.coretti@polito.it>
2021-04-24 15:50:49 +0200

    Implemented phi_max and phi_min.

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 
 double Rad2Deg(double phi_rad);
 double Deg2Rad(double phi_deg);
+void Service(double h, double delta_L, double phi);
 
 int main(void) {
 
@@ -15,40 +16,32 @@ int main(void) {
    double h = 0.;
    double delta_L = 0.;
 
-   // output variables
-   double theta = 0.;
-   double delta_court = 0.;
-   double delta_net = 0.;
-
    // Local variables
-   double phi = 0.;
    double phi_max = 0.;
    double phi_max_deg = 0.;
-   double theta_deg = 0.;
+   double phi_min = 0.;
+   double phi_min_deg = 0.;
 
-   printf("Insert the height in meters at which the ball is hit:\n");
+   printf("\nInsert the height in meters at which the ball is hit:\n");
    scanf("%lf", &h);
    printf("h = %lf\n", h);
 
    phi_max = atan((delta_L + .5*LCOURT + LSERVICE)/h);
    phi_max_deg = Rad2Deg(phi_max);
-   // printf("Maximum angle with respect to the vertical in degrees at which the ball can be hit is: %lf\n", phi_max_deg);
+   phi_min = .5*M_PI - atan((h - HNET)/(delta_L + .5*LCOURT));
+   phi_min_deg = Rad2Deg(phi_min);
 
-   printf("Insert the angle with respect to the vertical in degrees at which the ball is hit:\n");
+   printf("Maximum angle with respect to the vertical in degrees at which the ball can be hit: %lf\n", phi_max_deg);
+   printf("Minimum angle with respect to the vertical in degrees at which the ball can be hit: %lf\n", phi_min_deg);
+
+   Service(h, delta_L, phi_max_deg);
+   Service(h, delta_L, phi_min_deg);
+
+   printf("\nInsert the angle with respect to the vertical in degrees at which the ball is hit:\n");
    scanf("%lf", &phi_deg);
    printf("phi = %lf\n", phi_deg);
 
-   phi = Deg2Rad(phi_deg);
-
-   theta = M_PI/2. - phi;
-   delta_court = (delta_L + .5*LCOURT + LSERVICE) - h*tan(phi);
-   delta_net = (LSERVICE - delta_court)*tan(theta) - HNET;
-
-   theta_deg = Rad2Deg(theta);
-
-   printf("\nThe ball hit the court with an angle of %lf degrees with respect to the floor.\n", theta_deg);
-   printf("The ball hit the court at %lf meters from the service line.\n", delta_court);
-   printf("The ball passed %lf meters above the net.\n", delta_net);
+   Service(h, delta_L, phi_deg);
 
    return 0;
 }
@@ -61,4 +54,28 @@ double Rad2Deg(double phi_rad) {
 double Deg2Rad(double phi_deg) {
 
    return M_PI/180.*phi_deg;
+}
+
+void Service(double h, double delta_L, double phi_deg) {
+
+   double theta = 0.;
+   double delta_court = 0.;
+   double delta_net = 0.;
+
+   double phi = 0.;
+   double theta_deg = 0.;
+
+   phi = Deg2Rad(phi_deg);
+
+   theta = .5*M_PI - phi;
+   delta_court = (delta_L + .5*LCOURT + LSERVICE) - h*tan(phi);
+   delta_net = (LSERVICE - delta_court)*tan(theta) - HNET;
+
+   theta_deg = Rad2Deg(theta);
+
+   printf("\nThe ball hit the court with an angle of %lf degrees with respect to the floor.\n", theta_deg);
+   printf("The ball hit the court at %lf meters from the service line.\n", delta_court);
+   printf("The ball passed %lf meters above the net.\n", delta_net);
+
+   return;
 }
